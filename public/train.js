abee6002bed3d48f829549736656b8c408f13e9c
plat2eau <akhilergo@gmail.com>
2019-07-18 13:23:48 +0530

    bugfixes and added support for banks

--- public/train.js
+++ public/train.js
@@ -1,4 +1,3 @@
-
 var mobilenet;
 var classifier;
 let loss;
@@ -7,167 +6,207 @@ let img1;
 let data = {};
 let canvas;
 var selectedFile;
-const googleImages = 108;                                                         
-const amazonImages = 103;                                                         
-const paypalImages = 153;                                                         
-const facebookImages = 211;                                                       
-const dropboxImages = 133;           
+const googleImages = 108;
+const amazonImages = 103;
+const paypalImages = 153;
+const facebookImages = 211;
+const dropboxImages = 133;
+const barodaImages = 136;
+const canaraImages = 373;
+const hdfcImages = 246;
+const iciciImages = 274;
+const idbiImages = 157;
+const sbiImages = 308;
 const width = 400;
 const height = 400;
 var drozone;
 var count = 0;
+var imagesCompleted = 0;
+var totalImages = googleImages + amazonImages + paypalImages + facebookImages + dropboxImages + barodaImages + canaraImages + hdfcImages + iciciImages + idbiImages + sbiImages;
 
 function preload() {
-  data = loadJSON('json_ml5data.json', jsonReady);
+    data = loadJSON('json_ml5data.json', jsonReady);
 }
 
-function load_imgs(label, nimgs) {
-  for (let i = 0; i < nimgs; i++) {
-    imgpath = data.children[label].children[i].path;
-    type = data.children[label].children[i].type;
-    img = createImg(imgpath).hide();
-    console.log(imgpath + " loading " + type);
-    classifier.addImage(img, type);
-  }
-}
+function load_imgs(label, nimgs, imagesCallback) {
+    new Promise((resolve, reject) => {
+        for (let i = 0; i < nimgs; i++) {
+            const imageData = data.children[label].children[i]
+            const image = document.createElement("img")
+            image.src = "/all_data/" + imageData.path
+            image.onload = () => {
+                const type = imageData.type;
+                classifier.addImage(image, type, (res) => {
+                    console.log(image)
+                    imagesCompleted += 1;
+                    console.log("[ INFO ] : " + imagesCompleted + " of " + totalImages + " : " + ((imagesCompleted / totalImages) * 100).toFixed(2) + "%");
+                    if (i == (nimgs - 1)) {
+                        resolve()
+                    }
+                })
+            }
+            image.remove()
+        }
+    }).then(() => {
+        imagesCallback(data.children[label].datei)
+    })
 
+
+}
 // Change the status when the model loads.
 function modelReady() {
-  //select('#modelStatus').html('MobileNet Loaded!')
-  console.log('Model loaded');
-  select('.progress-bar').hide();
+    //select('#modelStatus').html('MobileNet Loaded!')
+    console.log('Model loaded');
+    select('.progress-bar').hide();
 }
 
 function customclassifier() {
-  console.log("custom classifier is loaded");
+    console.log("custom classifier is loaded");
 }
 
-function classifierReady () {
-  console.log("classifier is loaded");
-  //If you want to load a pre-trained model at the start
+function classifierReady() {
+    console.log("classifier is loaded");
+    //If you want to load a pre-trained model at the start
 }
 
 // Change the status when the model loads.
 function jsonReady() {
-  console.log('json loaded');
+    console.log('json loaded');
+    console.log(data);
 }
 
 function whileTraining(loss) {
-  if (loss == null) {
-    console.log('Training Complete');
-    //classifier.classify(gotResults);
-  } else {
-    console.log(loss);
-  }
+    if (loss == null) {
+        console.log('Training Complete');
+        //classifier.classify(gotResults);
+    } else {
+        console.log(loss);
+    }
+}
+
+function imagesCallback(className) {
+    console.log("[ INFO ] : All " + className + " images loaded");
+
 }
 
 function load_data() {
-  load_imgs(0, googleImages);
-  load_imgs(1, amazonImages);
-  load_imgs(2, paypalImages);
-  load_imgs(3, facebookImages);
-  load_imgs(4, dropboxImages);
+    try {
+        load_imgs(0, amazonImages, imagesCallback);
+        load_imgs(1, barodaImages, imagesCallback);
+        load_imgs(2, canaraImages, imagesCallback);
+        load_imgs(3, dropboxImages, imagesCallback);
+        load_imgs(4, facebookImages, imagesCallback);
+        load_imgs(5, googleImages, imagesCallback);
+        load_imgs(6, hdfcImages, imagesCallback);
+        load_imgs(7, iciciImages, imagesCallback);
+        load_imgs(8, idbiImages, imagesCallback);
+        load_imgs(9, paypalImages, imagesCallback);
+        load_imgs(10, sbiImages, imagesCallback);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-function train() {
-  const options = {version: 1, epochs: 20, numLabels: 5, batchSize: 0.2 };
-  mobilenet = ml5.featureExtractor('MobileNet', options, modelReady)
-  classifier = mobilenet.classification(classifierReady)
-		.then(() => load_data())
-		.catch(function (err) {
-			console.log(err);
-		});
+function load_dependencies() {
+    (async() => {
+        const options = await { version: 1, epochs: 20, numLabels: 5, batchSize: 0.2 };
+        mobilenet = await ml5.featureExtractor('MobileNet', options, modelReady)
+        classifier = await mobilenet.classification(classifierReady)
+    })();
 }
 
 function setup() {
-  noCanvas();
-  train();
+    noCanvas();
+    load_dependencies();
 
-  trainButton = select('#train'); 
-  trainButton.mousePressed(function() {
-    classifier.train(whileTraining);
-  });
+    trainButton = select('#train');
+    trainButton.mousePressed(function() {
+        classifier.train(whileTraining);
 
-  saveButton = select('#save'); 
-  saveButton.mousePressed(function() {
-    classifier.save();
-    console.log('Custom Model saved!!');
-  });
+    });
 
-  document.getElementById("filepicker").addEventListener("change", dirread, false);
-  document.getElementById("files").addEventListener("change", dirread, false);
+    saveButton = select('#save');
+    saveButton.mousePressed(function() {
+        classifier.save();
+        console.log('Custom Model saved!!');
+    });
+
+    document.getElementById("load").addEventListener("click", load_data)
+
+    document.getElementById("filepicker").addEventListener("change", dirread, false);
+    document.getElementById("files").addEventListener("change", dirread, false);
 }
 
-function dirread (event) {
-  console.log("inside dirread");
-  let output = document.getElementById("listing");
-  let files = event.target.files;
-  
-  for (let i=0; i<files.length; i++) {
-    f = files[i];
-    // Only process image files.
-    if (!f.type.match('image.*')) {
-      console.log("these files did not match");
-      continue;
+function dirread(event) {
+    console.log("inside dirread");
+    let output = document.getElementById("listing");
+    let files = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+        f = files[i];
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            console.log("these files did not match");
+            continue;
+        }
+
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return async function(e) {
+                img = await createImg(e.target.result).hide();
+                await img.size(224, 224);
+                let res = await testModel(img);
+                console.log(res);
+                let imgtag = document.createElement('img');
+                imgtag.src = e.target.result;
+                imgtag.setAttribute("width", "256");
+                imgtag.setAttribute("height", "256");
+                if ((count % 2) == 0) {
+                    $('#imageContainer').append(imgtag);
+                } else {
+                    $('#imageContainer2').append(imgtag);
+                }
+                //let listtag = document.createElement('ol');
+                let listtag = document.createElement('p');
+                //await img.parent(img_cont);
+                for (var i = 0; i < 5; i++) {
+                    //listtag.append(`<li> ${res[i].label}: ${round(res[i].confidence * 100) + '%'} </li>`);
+                    listtag.append(`${res[i].label}: ${round(res[i].confidence * 100) + '%'}  `);
+                    //var li = createElement('li', res[i].label + "   " + round(res[i].confidence * 100) + '%');
+                    //li.parent(pred_cont);
+                }
+
+                if ((count % 2) == 0) {
+                    $('#imageContainer').append(listtag);
+                } else {
+                    $('#imageContainer2').append(listtag);
+                }
+
+                count = count + 1;
+            };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
     }
-
-    var reader = new FileReader();
-    // Closure to capture the file information.
-    reader.onload = (function(theFile) {
-      return async function(e) {
-        img = await createImg(e.target.result).hide();
-        await img.size(224, 224);
-        let res = await testModel(img);
-        console.log(res);
-        let imgtag = document.createElement('img');
-        imgtag.src = e.target.result;
-        imgtag.setAttribute("width", "256");
-        imgtag.setAttribute("height", "256");
-        if ((count % 2) == 0) {
-          $('#imageContainer').append(imgtag);
-        } else {
-          $('#imageContainer2').append(imgtag);
-        }
-        //let listtag = document.createElement('ol');
-        let listtag = document.createElement('p');
-        //await img.parent(img_cont);
-        for (var i=0; i < 5; i++) {
-          //listtag.append(`<li> ${res[i].label}: ${round(res[i].confidence * 100) + '%'} </li>`);
-          listtag.append(`${res[i].label}: ${round(res[i].confidence * 100) + '%'}  `);
-          //var li = createElement('li', res[i].label + "   " + round(res[i].confidence * 100) + '%');
-          //li.parent(pred_cont);
-        }
-        
-        if ((count % 2) == 0) {
-          $('#imageContainer').append(listtag);
-        } else {
-          $('#imageContainer2').append(listtag);
-        }
-        
-        count = count+1;
-      };
-    })(f);
-
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-  }
 }
 
 async function testModel(img) {
-  // Get a prediction for that image
-  let res = await classifier.classify(img, function(err, result) {
-    console.log(result);
-    return result;
-  });
+    // Get a prediction for that image
+    let res = await classifier.classify(img, function(err, result) {
+        console.log(result);
+        return result;
+    });
 
-  return res;
+    return res;
 }
 
 function draw() {
-  fill(255);
-  noStroke();
-  textSize(25);
-  textAlign(CENTER);
-  text('Drag an image for prediction', 200, 200);
-  noLoop();
+    fill(255);
+    noStroke();
+    textSize(25);
+    textAlign(CENTER);
+    text('Drag an image for prediction', 200, 200);
+    noLoop();
 }
